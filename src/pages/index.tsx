1cd5b1f25154829fde19e132b451b72b62ae1d64
Benny Hernandez <mail@bennyhernandez.com>
2022-07-30 13:36:34 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Coinfolio</title>
+        <meta name="description" content="Crypto portfolio app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
+        app
+      </main>
+    </>
+  );
+};
+
+export default Home;
