297fa6f4818f04fa9fe8a2d83d5885b61fae89d2
Benny Hernandez <mail@bennyhernandez.com>
2022-08-01 13:38:04 -0400

    added context helpers

--- src/types/context.d.ts
+++ src/types/context.d.ts
@@ -1,6 +1,5 @@
 type Theme = "light" | "dark";
 
 type ContextProps = {
-  initialTheme: Theme;
   children: React.ReactNode;
 };
--- src/utils/contextHelpers.ts
+++ src/utils/contextHelpers.ts
@@ -1,29 +1,27 @@
-export function getInitialTheme(): Theme {
-  if (typeof window !== "undefined" && window.localStorage) {
-    // get theme mode
-    const storedPreferences = window.localStorage.getItem(
-      "color-theme"
-    ) as Theme;
-    // check if theme mode exist
-    if (typeof storedPreferences === "string") return storedPreferences;
-
-    // check if media is set to dark
-    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
-    if (userMedia.matches) return "dark";
-  }
-
-  return "light";
-}
-
 export function rawSetTheme(theme: Theme) {
-  const root = window.document.documentElement;
-  const isDark = theme === "dark";
+  if (typeof window !== "undefined") {
+    const root = window.document.documentElement;
+    const isDark = theme === "dark";
 
-  // add class to root
-  root.classList.remove(isDark ? "light" : "dark");
-  root.classList.add(theme);
+    // add class to root
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(theme);
 
-  // save to localStorage
-
-  localStorage.setItem("color-theme", theme);
+    localStorage.setItem("color-theme", theme);
+  }
 }
+
+export const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("color-theme") as Theme;
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+
+    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userMedia.matches) {
+      return "dark";
+    }
+  }
+  return "light";
+};
