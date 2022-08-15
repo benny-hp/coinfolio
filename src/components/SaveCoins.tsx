a4468d5d8c1dfbcddca5e79d41064c130416f7bb
Benny Hernandez <mail@bennyhernandez.com>
2022-08-15 15:42:09 -0400

    Created account page

---
+++ src/components/SaveCoins.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { trpc } from "../utils/trpc";
+
+const SaveCoins = () => {
+  const { data: coins } = trpc.useQuery(["coin.getAll"]);
+  const coinCtx = trpc.useContext();
+  const { mutate } = trpc.useMutation(["coin.remove"], {
+    onSuccess() {
+      coinCtx.invalidateQueries(["coin.getAll"]);
+    },
+  });
+
+  const removeCoin = (id: string) => {
+    mutate({ id });
+  };
+
+  return (
+    <div>
+      {coins?.length === 0 ? (
+        <p>
+          You don&apos;t have any coins saved. Please save a coin to add it your
+          watch list.{" "}
+          <Link href="/">
+            <a>Click here to search coins.</a>
+          </Link>
+        </p>
+      ) : (
+        <table className="w-full border-collapse text-center">
+          <thead>
+            <tr className="border-b">
+              <th className="px-4">Rank #</th>
+              <th className="text-left">Coin</th>
+              <th className="text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins &&
+              coins.map((coin, index) => (
+                <tr key={index} className="h-[60px] overflow-hidden">
+                  <td>{coin?.rank}</td>
+                  <td>
+                    <Link href={`/coin/${coin?.id}`}>
+                      <a className="flex items-center">
+                        <img className="w-8 mr-4" src={coin?.image} alt="/" />
+                        <div>
+                          <p className="hidden sm:table-cell">{coin?.name}</p>
+                          <p className="text-left text-sm">
+                            {coin?.symbol?.toUpperCase()}
+                          </p>
+                        </div>
+                      </a>
+                    </Link>
+                  </td>
+                  <td className="pl-8">
+                    <AiOutlineClose
+                      onClick={() => removeCoin(coin.id)}
+                      className="cursor-pointer"
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default SaveCoins;
--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,7 +1,68 @@
-import { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import Head from "next/head";
 
-const Account: NextPage = () => {
-  return <div>Account</div>;
+import SaveCoins from "../components/SaveCoins";
+import { getServerSession } from "../utils/getServerSession";
+
+type Props = {
+  data: Session;
+};
+const Account = ({ data }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>Coinfolio | Account</title>
+        <meta name="description" content="Crypto portfolio app" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="max-w-[1140px] mx-auto">
+        <div className="flex justify-between items-center my-4 py-8 rounded-div">
+          <div>
+            <h1 className="text-2xl font-bold">Account</h1>
+            <div>
+              <p>Welcome, {data?.user?.name}</p>
+            </div>
+          </div>
+          <div>
+            <button
+              className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+              onClick={() => signOut({ callbackUrl: "/", redirect: true })}
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center my-12 py-8 rounded-div">
+          <div className="w-full min-h-[300px] sm:min-h-[44vh]">
+            <h1 className="text-2xl font-bold py-4">Watch List</h1>
+            <SaveCoins />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+      props: {
+        data: null,
+      },
+    };
+  }
+  return {
+    props: {
+      data: session,
+    },
+  };
 };
 
 export default Account;
