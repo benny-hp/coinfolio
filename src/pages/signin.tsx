92ee698d74cbe706f6f3667c8a1c291b96ffc7f1
Benny Hernandez <mail@bennyhernandez.com>
2022-08-15 15:33:57 -0400

    Created sign in page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from "next";
+import { signIn } from "next-auth/react";
 
 const Signin: NextPage = () => {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center max-w-[400px] mx-auto min-h-[72vh] px-4">
+        <h1 className="text-2xl font-bold mb-4 ">Sign In</h1>
+        <button
+          onClick={() =>
+            signIn("google", { callbackUrl: "/account", redirect: true })
+          }
+          className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl"
+        >
+          Sign In With Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
