31fd33fc5514ceefe2816ffc3bcb44866b62b06f
Benny Hernandez <mail@bennyhernandez.com>
2022-08-15 15:31:18 -0400

    Updated nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ThemeToggle from "./ThemeToggle";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const { data: session } = useSession();
+
+  const lockScroll = React.useCallback(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  const unlockScroll = React.useCallback(() => {
+    document.body.style.overflow = "";
+  }, []);
 
   function handleNav() {
+    if (!nav) {
+      lockScroll();
+    }
+    if (nav) {
+      unlockScroll();
+    }
     setNav(!nav);
   }
   return (
@@ -19,13 +35,27 @@ const Navbar = () => {
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div className="hidden md:block">
-        <Link href="/signin">
-          <a className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl">
-            Sign In
-          </a>
-        </Link>
-      </div>
+      {session?.user ? (
+        <div>
+          <Link href="/account">
+            <a className="hidden md:inline p-4">Account</a>
+          </Link>
+          <button
+            className="hidden md:inline bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+            onClick={() => signOut({ callbackUrl: "/", redirect: true })}
+          >
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link href="/signin">
+            <a className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl">
+              Sign In
+            </a>
+          </Link>
+        </div>
+      )}
       {/* Menu Icon */}
       <div className="block md:hidden cursor-pointer z-10" onClick={handleNav}>
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -35,18 +65,18 @@ const Navbar = () => {
         className={
           nav
             ? "md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10"
-            : "fixed left-[100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300"
+            : "fixed w-full bg-primary left-[100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300"
         }
       >
         <ul className="w-full p-4">
           <li className="border-b py-6">
             <Link href={"/"}>
-              <a>Home</a>
+              <a onClick={handleNav}>Home</a>
             </Link>
           </li>
           <li className="border-b py-6">
             <Link href={"/account"}>
-              <a>Account</a>
+              <a onClick={handleNav}>Account</a>
             </Link>
           </li>
           <li className="py-6">
@@ -54,13 +84,22 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="flex flex-col w-full p-4">
-          <Link href="/signin">
-            <a>
-              <button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
-                Sign In
-              </button>
-            </a>
-          </Link>
+          {session?.user ? (
+            <button
+              className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl"
+              onClick={() => signOut({ callbackUrl: "/", redirect: true })}
+            >
+              Sign Out
+            </button>
+          ) : (
+            <Link href="/signin">
+              <a onClick={handleNav}>
+                <button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+                  Sign In
+                </button>
+              </a>
+            </Link>
+          )}
         </div>
       </div>
     </div>
